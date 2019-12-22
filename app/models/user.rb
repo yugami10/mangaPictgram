d30faec9512ad3761e9f6dea019990ebb31903a6
名前 <>
2019-12-22 05:42:47 +0000

    mangaPictgram_いいね_新規登録_画像投稿_ログイン_画像一覧画面

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  
+  has_secure_password
+  
+  has_many :topics
+  has_many :favorites
+  has_many :favorite_topics, through: :favorites, source: 'topic'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'sessions/new'
+  get 'pages/index'
+  root 'pages#index'
+  
+  resources :users
+  resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
